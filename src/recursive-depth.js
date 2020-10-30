d5c1fdb97448540c43a6a2d765970ba840fc91bc
VictoriaKacer <kacer1703@gmail.com>
2020-10-30 13:39:48 +0300

    feat: add tasc

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    
   }
-};
+  calculateDepth(arr) {
+    let counter = 1;
+    let depth = 1;
+    for (let item of arr) {
+      if (Array.isArray(item)) {
+        counter = 1 + this.calculateDepth(item);
+       
+        
+      } 
+     
+    }
+    if (counter > depth) {
+      depth = counter; 
+    }
+    
+    return depth;
+  }
+  
+  };
+  // 1 не прохожу
+
+
+  // 1 не прохожу
